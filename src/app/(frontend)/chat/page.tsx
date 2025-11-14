65c2772a860fdb3a53b33c8f94eafd8baaca792b
rafdspace <rafdian.ramadhan@gmail.com>
2025-11-14 14:31:57 +0700

    fix: adjust chat layout

--- src/app/(frontend)/chat/page.tsx
+++ src/app/(frontend)/chat/page.tsx
@@ -2,7 +2,7 @@ import { ChatContainer } from "@/components/chat/ChatContainer";
 
 export default async function ChatPage() {
   return (
-    <div className="flex-1 flex gap-6 flex-col justify-between">
+    <div className="flex-1 flex gap-10 flex-col justify-between">
       <div className="text-left md:text-center">
         <h2 className="text-3xl font-bold">Ask Me Anything 💬</h2>
         <p className="text-gray-600 dark:text-gray-400">
